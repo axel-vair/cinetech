9462b4ea78450087f64740e0bad998dfcc4465aa
axelvair <axel.vair@laplateforme.io>
2023-05-22 17:43:52 +0200

    add some style on home page

--- scripts/home-movies.js
+++ scripts/home-movies.js
@@ -13,7 +13,7 @@ fetch('https://api.themoviedb.org/3/trending/movie/day?language=fr-Fr', options)
             const title = item.title;
             const pathImg = "https://image.tmdb.org/t/p/w500"
             const image = pathImg + item.poster_path;
-            const movie = `<li><img src="${image}"><h2>${title}</h2></li>`
+            const movie = `<li><img class="poster" src="${image}" alt="${title}"></li>`
             document.querySelector('.trending-movies').innerHTML += movie;
         })
     })
@@ -29,7 +29,7 @@ fetch('https://api.themoviedb.org/3/trending/tv/day?language=fr-FR', options)
             const titleSeries = item.name;
             const pathImgSeries = "https://image.tmdb.org/t/p/w500"
             const imageSeries = pathImgSeries + item.poster_path;
-            const serie = `<li><img src="${imageSeries}"><h2>${titleSeries}</h2></li>`
+            const serie = `<li><img class="poster" src="${imageSeries}" alt="${titleSeries}"></li>`
             document.querySelector('.trending-series').innerHTML += serie;
         })
     })
@@ -41,18 +41,17 @@ let searchBar = document.getElementById('search');
 // Listening searchbar on keyup
 searchBar.addEventListener('keyup', () => {
     let keypress = searchBar.value;
-    fetch(`https://api.themoviedb.org/3/search/multi?query=${keypress}&include_adult=true&language=fr-FR&page=1`, options)
+    fetch(`https://api.themoviedb.org/3/search/multi?query=${keypress}&include_adult=false&language=fr-FR&page=1`, options)
         .then(response => response.json())
         .then(data => {
             const search = data.results;
-            console.log(search)
             search.map((item) => {
                 const titleSearch = item.title ? item.title : item.name;
-                console.log(item.name)
                 const pathImgSearch = "https://image.tmdb.org/t/p/w500"
                 const imageSearch = pathImgSearch + item.poster_path;
-                const searchValue = `<li><img src="${imageSearch}"><h2>${titleSearch}</h2></li>`
+                const searchValue = `<li><img class="poster" src="${imageSearch}" alt="${titleSearch}"></li>`
                 document.querySelector('.search').innerHTML += searchValue;
             })
 
 })})
+
